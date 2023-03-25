6577356367e24f3788acd5675616dae2d07aa4b7
Mac12880 <marc.juan6.mj@gmail.com>
2023-03-25 19:37:59 +0800

    Fix Responsive Header

---
+++ main.js
@@ -0,0 +1,14 @@
+const toggleBtn = document.querySelector(".toggle_btn");
+const toggleBtnIcon = document.querySelector(".toggle_btn i");
+const dropDownMenu = document.querySelector("#dropdownnavBar");
+
+toggleBtn.onclick = function () {
+    if (dropDownMenu.style.display === "none") {
+        toggleBtnIcon.classList.replace("fa-bars", "fa-xmark");
+        toggleBtnIcon.classList.add("fa-solid");
+        dropDownMenu.style.display = "block";
+    } else {
+        toggleBtnIcon.classList.replace("fa-xmark", "fa-bars");
+        dropDownMenu.style.display = "none";
+    }
+}
